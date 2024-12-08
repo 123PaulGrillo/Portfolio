4d02fa71c2a39cde666353d8a2f27aae5827cf38
123PaulGrillo <paulfto@outlook.es>
2024-12-07 20:35:26 -0700

    Ajustando el menú

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Referencias a los elementos
   const menuButton = document.getElementById("menu-button");
   const menuLinks = document.getElementById("menu-links");
 
-  menuButton.addEventListener("click", () => {
-    if (menuLinks.classList.contains("hidden")) {
-      menuLinks.classList.remove("hidden");
-      menuLinks.classList.add("flex");
-    } else {
-      menuLinks.classList.remove("flex");
-      menuLinks.classList.add("hidden");
-    }
-  });
+  // Asegurarse de que los elementos existen antes de añadir eventos
+  if (menuButton && menuLinks) {
+    menuButton.addEventListener("click", () => {
+      // Alternar clases para mostrar/ocultar el menú
+      menuLinks.classList.toggle("hidden");
+      menuLinks.classList.toggle("flex");
+    });
+  } else {
+    console.error("No se encontró el botón de menú o el contenedor de enlaces");
+  }
 });
